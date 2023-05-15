e69de409f3781962a49ae6f71c228bcb11df5886
Anoop Gupta <anoop@Anoops-MacBook-Air.local>
2023-05-15 18:33:40 +0530

    added skelton for controller & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NewsController } from './news/news.controller';
+import { GnewsService } from './gnews/gnews.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, NewsController],
+  providers: [AppService, GnewsService],
 })
 export class AppModule {}
